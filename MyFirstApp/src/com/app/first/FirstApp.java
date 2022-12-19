6a702612cd3117070bc189cafe29574ae77ce457
biswajit1222 <117624099+biswajit1222@users.noreply.github.com>
2022-12-19 12:28:17 +0530

    Update FirstApp.java
    c

--- MyFirstApp/src/com/app/first/FirstApp.java
+++ MyFirstApp/src/com/app/first/FirstApp.java
@@ -2,7 +2,7 @@ package com.app.first;
 
 public class FirstApp {
 public static void main(String[] args) {
-	System.out.println("hi i am java:::::::");
+	System.out.println("hi i am java:::::::11111");
 	System.out.println("here i am");
 	System.out.println("xxxxxx");
 	System.out.println("abcdegh");
